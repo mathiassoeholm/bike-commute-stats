e8d698602623f5b3150ca801cf8b57c9af1fc1b1
Mathias Soeholm <mathiassoeholm@gmail.com>
2020-07-20 13:43:23 +0200

    Get access token from Strava

--- src/build-site.tsx
+++ src/build-site.tsx
@@ -1,11 +1,40 @@
-import React from "react";
+import * as React from "react";
 import ReactDOMServer from "react-dom/server";
 import { Home } from "./site/home";
 import { writeFileSync, mkdirSync, existsSync } from "fs";
+import { URLSearchParams } from "url";
+import fetch from "node-fetch";
 
-const html = ReactDOMServer.renderToString(<Home />);
+require("dotenv").config();
 
-if (!existsSync("./build")) {
-  mkdirSync("build");
-}
-writeFileSync("./build/index.html", html);
+(async () => {
+  const {
+    STRAVA_CLIENT_ID,
+    STRAVA_REFRESH_TOKEN,
+    STRAVA_CLIENT_SECRET,
+  } = process.env;
+
+  if (!STRAVA_CLIENT_ID || !STRAVA_REFRESH_TOKEN || !STRAVA_CLIENT_SECRET) {
+    throw new Error("Some of the required environment variables were not set");
+  }
+
+  const params = new URLSearchParams();
+  params.append("client_id", STRAVA_CLIENT_ID);
+  params.append("client_secret", STRAVA_CLIENT_SECRET);
+  params.append("grant_type", "refresh_token");
+  params.append("refresh_token", STRAVA_REFRESH_TOKEN);
+
+  const response = await fetch("https://www.strava.com/api/v3/oauth/token", {
+    method: "POST",
+    body: params,
+  });
+
+  const accessToken = (await response.json()).access_token;
+
+  const html = ReactDOMServer.renderToString(<Home />);
+
+  if (!existsSync("./build")) {
+    mkdirSync("build");
+  }
+  writeFileSync("./build/index.html", html);
+})();
